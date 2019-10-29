c1bfbbfcd1bf71e5ce49d429906a7a4284d461fa
miguelrc <mirodriguez11@gmail.com>
2019-10-29 22:56:01 +0100

    Handle graphql errors and invalid token error redirects to login

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import {ApolloClient, ApolloLink, InMemoryCache, HttpLink} from "apollo-boost";
 import { ApolloProvider } from '@apollo/react-hooks';
+import { onError } from 'apollo-link-error';
 import { BrowserRouter } from "react-router-dom";
+import { createBrowserHistory } from "history";
 import Translator from './contextProviders/Translator';
 import ThemeContainer from './containers/ThemeContainer';
+import { ToastContainer, toast } from 'react-toastify';
 import AppRouter from "./containers/AppRouter";
+
+let toastId;
+const bHistory = createBrowserHistory();
 
 const httpLink = new HttpLink({
     uri: process.env.REACT_APP_API_URL
@@ -26,16 +32,37 @@ const authLink = new ApolloLink((operation, forward) => {
 	return forward(operation);
 });
 
+const errorLink = onError(({ graphQLErrors, networkError, operation, response, forward}) => {
+    console.error('ERROR_LINK');
+	console.error(graphQLErrors);
+	console.error(networkError);
+
+    if(graphQLErrors[0].code === 401){
+        if (!toast.isActive(toastId)) {
+            toastId = toast.error('Invalid Token', {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true
+            });
+            bHistory.replace('/');
+        }
+    }
+});
+
 const client = new ApolloClient({
-	link: authLink.concat(httpLink),
+	link: ApolloLink.from([errorLink, authLink, httpLink]),
     cache: new InMemoryCache()
 });
 
-function App() {
+function App({history}) {
 	return (
 		<ApolloProvider client={client}>
             <ThemeContainer>
                 <Translator>
+                    <ToastContainer/>
                     <BrowserRouter>
                         <AppRouter />
                     </BrowserRouter>
