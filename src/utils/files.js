e3cfb319c2f49c90a5aa04229e608a357914a234
miguelrc <mirodriguez11@gmail.com>
2019-11-13 22:36:39 +0100

    Util function that reduce size of product image uploaded

--- src/utils/files.js
+++ src/utils/files.js
@@ -4,3 +4,27 @@ export const toBase64 = file => new Promise((resolve, reject) => {
     reader.onload = () => resolve(reader.result);
     reader.onerror = error => reject(error);
 });
+
+export const reduceBase64Size = (base64) => {
+    return new Promise((resolve, reject) => {
+        let canvas = document.createElement("canvas");
+        let ctx = canvas.getContext("2d");
+
+        canvas.width = 120; // target width
+        canvas.height = 120; // target height
+
+        let image = new Image();
+
+        image.onload = function() {
+            ctx.drawImage(image, 
+                0, 0, image.width, image.height, 
+                0, 0, canvas.width, canvas.height
+            );
+            const resizedBase64 = canvas.toDataURL();
+
+            resolve(resizedBase64);
+        }
+
+        image.src = base64;
+    })
+}
